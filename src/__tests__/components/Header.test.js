b3a69c2ea91210b76be8507aa64d117a648c2ff6
Arian Racca <arianracca@gmail.com>
2023-07-15 20:04:19 -0300

    Test Header

---
+++ src/__tests__/components/Header.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Header from '../../components/Header';
+import Form from '../../components/Form';
+import Navigation from '../../components/Navigation';
+
+jest.mock('../../components/Form', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div>Mocked Form</div>),
+}));
+
+jest.mock('../../components/Navigation', () => ({
+  __esModule: true,
+  default: jest.fn(() => <div>Mocked Navigation</div>),
+}));
+
+describe('Header', () => {
+  it('should render the Header component with mocked Form and Navigation', () => {
+    const historyMock = {};
+    const handleSubmitMock = jest.fn();
+    render(<Header history={historyMock} handleSubmit={handleSubmitMock} />);
+    
+    expect(Form).toHaveBeenCalledTimes(1);
+    expect(Navigation).toHaveBeenCalledTimes(1);
+  });
+});
